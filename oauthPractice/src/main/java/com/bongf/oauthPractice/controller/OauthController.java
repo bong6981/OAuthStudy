70f33c052a42281ade2ef7ba0ca08ff244ba6b9a
bong6981 <kbk5190@naver.com>
2021-04-27 21:54:58 +0900

    feat : Seperate secret Information
    - 깃헙에 전달할 client_id와 client_secret은 다른 properties 파일로 분리

--- oauthPractice/src/main/java/com/bongf/oauthPractice/controller/OauthController.java
+++ oauthPractice/src/main/java/com/bongf/oauthPractice/controller/OauthController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -12,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,14 +22,18 @@ import org.springframework.web.client.RestTemplate;
 public class OauthController {
     Logger logger = LoggerFactory.getLogger(OauthController.class);
     private String code;
+    private final Environment environment;
 
+    public OauthController(Environment environment) {
+        this.environment = environment;
+    }
 
     @GetMapping("/login/oauth2/code/github")
     public String code(String code) {
         this.code = code;
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("client_id", "5c74787053b981c45a96");
-        params.add("");
+        params.add("client_id", environment.getProperty("client.id"));
+        params.add("client_secret", environment.getProperty("client.secret"));
         params.add("code", code);
         params.add("redirect_url", "http://localhost:8080/login/oauth2/code/github");
 
