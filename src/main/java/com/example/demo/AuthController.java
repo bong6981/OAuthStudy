ccc03deff9f43204445082da6ac17b656baa7684
PizzaCola-K <PizzaCola.K@gmail.com>
2021-04-29 15:40:45 +0900

    refactor: Auth and HomeController

--- src/main/java/com/example/demo/AuthController.java
+++ src/main/java/com/example/demo/AuthController.java
@@ -1,70 +1,58 @@
 package com.example.demo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.MultiValueMapAdapter;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpSession;
 import java.net.URI;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
+import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 public class AuthController {
 
     private final Logger logger = LoggerFactory.getLogger(AuthController.class);
-    private final URI GITHUB_ACCESS_TOKEN_URI = UriComponentsBuilder
-            .fromUriString("https://github.com/login/oauth/access_token")
-            .build().toUri();
 
-    private final URI GITHUB_USER_URI = UriComponentsBuilder
-            .fromUriString("https://api.github.com/user")
-            .build().toUri();
+    private final String GITHUB_ACCESS_TOKEN_URI = "https://github.com/login/oauth/access_token";
+    private final String GITHUB_USER_URI = "https://api.github.com/user";
 
-    private final URI HOME = UriComponentsBuilder
-            .fromUriString("/")
-            .build().toUri();
+    private final String CLIENT_ID;
+    private final String CLIENT_SECRET;
 
-    private final Environment environment;
+    //private final Environment environment;
 
     public AuthController(Environment environment) {
-        this.environment = environment;
+        //this.environment = environment;
+        CLIENT_ID = environment.getProperty("github.client.id");
+        CLIENT_SECRET = environment.getProperty("github.client.secret");
     }
 
     @GetMapping
-    public ResponseEntity<Void> auth(String code, HttpSession session) {
+    public String auth(String code, HttpSession session) {
         RestTemplate gitHubRequest = new RestTemplate();
-        String clientId = environment.getProperty("github.client.id");
-        String clientSecret = environment.getProperty("github.client.secret");
+        GithubAccessTokenResponse accessToken = getAccessToken(code, gitHubRequest)
+                .orElseThrow(() -> new RuntimeException("바디 없음"));
 
-        RequestEntity<GithubAccessTokenRequest> requestEntity = RequestEntity
-                .post(GITHUB_ACCESS_TOKEN_URI)
-                .header("Accept", "application/json")
-                .body(new GithubAccessTokenRequest(clientId, clientSecret, code));
-
-        ResponseEntity<GithubAccessTokenResponse> response = gitHubRequest
-                .exchange(requestEntity, GithubAccessTokenResponse.class);
-
-        GithubAccessTokenResponse accessToken = response.getBody();
-
-        if (accessToken == null) {
-            return ResponseEntity.badRequest().build();
-        }
         session.setAttribute("accessToken", accessToken.getAccessToken());
 
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(HOME);
-        return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
+        return "redirect:/";
+    }
+
+    private Optional<GithubAccessTokenResponse> getAccessToken(String code, RestTemplate gitHubRequest) {
+        RequestEntity<GithubAccessTokenRequest> request = RequestEntity
+                .post(GITHUB_ACCESS_TOKEN_URI)
+                .header("Accept", "application/json")
+                .body(new GithubAccessTokenRequest(CLIENT_ID, CLIENT_SECRET, code));
+
+        ResponseEntity<GithubAccessTokenResponse> response = gitHubRequest
+                .exchange(request, GithubAccessTokenResponse.class);
+
+        return Optional.ofNullable(response.getBody());
     }
 }
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,15 +3,27 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
+    private final String ACCESS_TOKEN = "accessToken";
+    private final String USER = "user";
+
     @GetMapping
     public String home(Model model, HttpSession session) {
-        model.addAttribute("user", session.getAttribute("accessToken"));
+        if (session.getAttribute(ACCESS_TOKEN) != null) {
+            model.addAttribute(USER, USER);
+        }
         return "index";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute(ACCESS_TOKEN);
+        return "redirect:/";
     }
 }
